9cfd36a3d74ce6be786c6a72950caeadc2cc6279
jxgohh <gohjoonxiang@gmail.com>
2026-01-21 22:19:32 +0800

    Added add and list function to Ultron along with minor fixes to lines

--- src/main/java/Ultron.java
+++ src/main/java/Ultron.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Ultron {
     private final static String LINE = "------------------------------------------------";
+    private static ArrayList<String> list = new ArrayList<>();
 
     private static void greet() {
+        System.out.println(LINE);
         System.out.println("Hello! I'm Ultron\nWhat can I do for you?");
         System.out.println(LINE);
     }
@@ -13,6 +16,18 @@ public class Ultron {
         System.out.println(LINE);
     }
 
+    private static void append(String str) {
+         list.add(str);
+    }
+
+    private static void printList() {
+        System.out.println(LINE);
+        for (int i = 0; i < list.size(); i++) {
+            String idStr = Integer.toString(i+1);
+            System.out.println(idStr + ". " + list.get(i));
+        }
+        System.out.println(LINE);
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -22,9 +37,14 @@ public class Ultron {
             if (line.equals("bye")) {
                 goodbye();
                 break;
+            } else if (line.equals("list")) {
+                printList();
+            } else {
+                append(line);
+                System.out.println(LINE);
+                System.out.println("added: " + line);
+                System.out.println(LINE);
             }
-            System.out.println(line);
-            System.out.println(LINE);
         }
         sc.close();
     }
