ac5a537e898c9eb2d7aa23f1f1813e7a6596f207
jxgohh <gohjoonxiang@gmail.com>
2026-01-21 22:01:37 +0800

    Added echo for Ultron

--- src/main/java/Ultron.java
+++ src/main/java/Ultron.java
@@ -1,14 +1,31 @@
+import java.util.Scanner;
+
 public class Ultron {
+    private final static String LINE = "------------------------------------------------";
+
     private static void greet() {
         System.out.println("Hello! I'm Ultron\nWhat can I do for you?");
-        System.out.println("------------------------------------------------");
+        System.out.println(LINE);
     }
+
     private static void goodbye() {
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("------------------------------------------------");
+        System.out.println(LINE);
     }
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         greet();
-        goodbye();
+        while (true) {
+            String line = sc.nextLine();
+            if (line.equals("bye")) {
+                goodbye();
+                break;
+            }
+            System.out.println(line);
+            System.out.println(LINE);
+        }
+        sc.close();
     }
 }
