d00904f992ce8b0e3fde2fcea42fb2b0f355729d
jxgohh <gohjoonxiang@gmail.com>
2026-01-21 21:49:58 +0800

    Added greeting and goodbye messages and renamed duke to ultron

---
+++ src/main/java/Ultron.java
@@ -0,0 +1,14 @@
+public class Ultron {
+    private static void greet() {
+        System.out.println("Hello! I'm Ultron\nWhat can I do for you?");
+        System.out.println("------------------------------------------------");
+    }
+    private static void goodbye() {
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("------------------------------------------------");
+    }
+    public static void main(String[] args) {
+        greet();
+        goodbye();
+    }
+}
