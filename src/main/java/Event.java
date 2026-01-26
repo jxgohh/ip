a292650a0a04d68040da0b3f6f3972c2bb89025b
jxgohh <gohjoonxiang@gmail.com>
2026-01-26 23:20:21 +0800

    Added new class Storage which handles storing and retrieval of the data necessary. File will be located in src/main/data. Corrupted files will return an empty list

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -6,6 +6,19 @@ public class Event extends Task{
         this.from = from;
     }
 
+    public Event(String description, boolean completed, String from) {
+        super(description, completed);
+        this.from = from;
+    }
+
+    public String getFrom() {
+        return this.from;
+    }
+    @Override
+    public String storageLine() {
+        return "E | " + (this.isDone ? "1" : "0") + " | " + this.description + " | " + this.from;
+    }
+
     @Override
     public String toString() {
         return "[E]" + super.toString() + " (from: " + from + ")";
